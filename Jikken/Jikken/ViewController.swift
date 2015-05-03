ec8bc6d63b07c54476154539779b706a4a840499
YoshifumiA <yoshifumiaosaki@gmail.com>
2015-05-03 17:57:12 +0900

    test commit

---
+++ Jikken/Jikken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jikken
+//
+//  Created by 青崎愛史 on 2015/05/03.
+//  Copyright (c) 2015年 青崎愛史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
